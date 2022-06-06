71435f7a3f2b31b5c7e13a489c82035f61794b2a
jjyjoy1 <jiyang.jiang@gmail.com>
2022-06-05 23:52:07 -0400

    new ebfiles

--- eb_install.sh
+++ eb_install.sh
@@ -7,7 +7,7 @@ module load Easybuild/4.15.1
 CFGS="To/Easybuild/easyconfigs/dir"
 #dbfile='f/foss/foss-2021b.eb'
 
-#Collect the uninstall module files
+#Collect the uninstall module files, as example for foss-2021b.eb, which contains long tools chain
 eb $CFGS/f/foss/foss-2021b.eb -D --modules-tool EnvironmentModules --module-syntax Tcl --installpath-software /home/jjy4/bin/ --installpath-modules /home/jjy4/bin/Modulefiles/ --allow-modules-tool-mismatch | grep 'module' | grep -ivw 'x' | awk '{print $4}' > foss2021b.txt
 
 #Using easybuild to install foss compiler, the latest version 2021b
